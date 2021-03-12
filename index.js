cc09a55d63dafcf5605dcbfe4ec79c2337f27d84
Omar Cabatbat <omar_cabatbat@yahoo.com>
2021-03-12 02:20:18 +0000

    Fetch flyover times for ISS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP ((error, ip) => {
   if (error) {
@@ -15,4 +16,15 @@ fetchCoordsByIP ('216.58.106.144', (error, data) => {
   }
   
   console.log('It worked! Returned coordinates:' , data);
-})
+});
+
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
+});
