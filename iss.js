0370dcc92e0bb4025cc6efc902c8cf3e832c450c
Omar Cabatbat <omar_cabatbat@yahoo.com>
2021-03-12 02:14:27 +0000

    Initial Commit

---
+++ iss.js
@@ -0,0 +1,39 @@
+const request = require('request');
+const fetchMyIP = function (callback) {
+  // use request to fetch IP address from JSON API
+  request('https://api.ipify.org/?format=json', (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    //if no error, return the IP
+
+    const data = JSON.parse(body);
+    callback(null, data);
+    
+  });
+};
+
+const fetchCoordsByIP = function (ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP. Response: ${body}`), null);
+      return;
+    }
+    //if no error, return data
+    const {latitude, longitude} = JSON.parse(body);
+    callback(null, { latitude, longitude });
+  });
+}
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
